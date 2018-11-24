8d92a4206d0e8046d55d3cf136e00012b9c1f197
paintdog <paintdog@users.noreply.github.com>
2018-11-24 22:06:01 +0100

    Update one_page_one_file.py

--- one_page_one_file.py
+++ one_page_one_file.py
@@ -1,20 +1,22 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
+import os
 
-# get the source
-input1 = PdfFileReader(open("__original__.pdf", "rb"))
-
+files = [file for file in os.listdir() if file.endswith(".pdf")]
 
 number = 1000
 
+for file in files:
+    # get the source
+    input1 = PdfFileReader(open(file, "rb"))
 
-# one file to x files (one page == one file)
-for page in range(input1.getNumPages()):
-    
-    output = PdfFileWriter()
-    output.addPage(input1.getPage(page))
+    # one file to x files (one page == one file)
+    for page in range(input1.getNumPages()):
+        
+        output = PdfFileWriter()
+        output.addPage(input1.getPage(page))
 
-    outputStream = open("{:04d}.pdf".format(number), "wb")
-    output.write(outputStream)
-    outputStream.close()
+        outputStream = open("{:04d}.pdf".format(number), "wb")
+        output.write(outputStream)
+        outputStream.close()
 
-    number += 1
+        number += 1
