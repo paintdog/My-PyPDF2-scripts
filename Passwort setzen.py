568927f50b667307b4884c04389949909080feef
paintdog <paintdog@users.noreply.github.com>
2019-03-07 22:47:16 +0100

    Update Passwort setzen.py

--- Passwort setzen.py
+++ Passwort setzen.py
@@ -6,28 +6,33 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 
 password = "PASSWORD"   # Change this to 1234 in production!!!
 
-t1 = datetime.datetime.now()
 
 winsound.Beep(2000,200)
 winsound.Beep(2000,200)
 
-file = [file for file in os.listdir() if (file.endswith(".pdf") or file.endswith(".PDF")) and file != 'document-output.pdf'][-1]
 
-# Quelle einlesen und Ausgabe vorbereiten
-output = PdfFileWriter()
-input_pdf = PdfFileReader(open(file, "rb"))
+files = [file for file in os.listdir() if not file.startswith("__protected ") and file.endswith(".pdf") or file.endswith(".PDF")]
 
-output.appendPagesFromReader(input_pdf)
-output.encrypt(password)
 
-# Neues Dokument ausgeben
-outputStream = open("document-output.pdf", "wb")
-output.write(outputStream)
-outputStream.close()
+for file in files:
 
-winsound.Beep(2000,200)
-winsound.Beep(2000,200)
+    t1 = datetime.datetime.now()
 
-t2 = datetime.datetime.now()
+    # Quelle einlesen und Ausgabe vorbereiten
+    output = PdfFileWriter()
+    input_pdf = PdfFileReader(open(file, "rb"))
 
-print("I needed  {}  seconds.".format((t2 - t1).total_seconds()))
+    output.appendPagesFromReader(input_pdf)
+    output.encrypt(password)
+
+    # Neues Dokument ausgeben
+    outputStream = open("__protected {}".format(file), "wb")
+    output.write(outputStream)
+    outputStream.close()
+
+    winsound.Beep(2000,200)
+    winsound.Beep(2000,200)
+
+    t2 = datetime.datetime.now()
+
+    print("I needed  {}  seconds.".format((t2 - t1).total_seconds()))
